fe39647bb7d41323fc914ee399604130556408c0
tss <sabasarwar1997@gmail.com>
2020-06-08 22:54:27 +0530

    bit manipulation:power of two

--- Leetcode/Leetcode June Challenge/08_Power_of_Two/main.cpp
+++ Leetcode/Leetcode June Challenge/08_Power_of_Two/main.cpp
@@ -46,3 +46,13 @@ public:
         
     }
 };
+
+// Solution using bit manipulation
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) 
+    {
+        return n>0 && (n & (n-1))==0;
+    }
+};
